faff9cf9596bedc566476dddc7521382cc9e9354
Sebastian Waisbrot <seppo0010@gmail.com>
2018-03-07 04:02:56 -0300

    better svr

--- svm.py
+++ svm.py
@@ -1,38 +1,32 @@
-import csv
 import math
-import random
-from sklearn import svm
 import sys
+import pandas as pd
+import numpy as np
+from sklearn import svm
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.metrics import mean_absolute_error
 
-columns = None
-data = []
-salary = []
-reader = csv.reader(sys.stdin)
-for row in reader:
-    if columns is None:
-        columns = row[:-1]
-    else:
-        data.append([float(x) for x in row[:-1]])
-        salary.append(float(row[-1]))
+data = pd.read_csv(sys.stdin)
 
-num_sample = 1000
-x = [[int(y * 100) for y in x] for x in data[:num_sample]]
-y = [int(x) for x in salary[:num_sample]]
-clf = svm.SVC()
-clf.fit(x, y)
+columns = data.axes[1][:-1]
+folds = KFold(n_splits=4)
+y = data.salary.values
+X = data.drop(['salary'], axis=1).values
+clf = svm.SVR(kernel='rbf', C=75000, gamma=3)
+clf.fit(X, y)
 
 errs = []
-for x, d in enumerate(data):
+for x, d in enumerate(X):
     expected = clf.predict([d])[0]
-    value = salary[x]
+    value = y[x]
     err2 = (expected - value) * (expected - value)
     errs.append((err2, x))
 
 errs.sort(key=lambda x: x[0], reverse=True)
-for (_, x) in errs[:5]:
-    d = data[x]
+for (_, x) in errs[:10]:
+    d = X[x]
     expected = clf.predict([d])[0]
-    print('expected: {}, value: {}'.format(round(expected), round(salary[x])))
+    print('expected: {}, value: {}'.format(round(expected), round(y[x])))
     print(' '.join('{}: {}'.format(col, d[i]) for i, col in enumerate(columns)))
 
 print(math.log(sum(e[0] for e in errs)))
